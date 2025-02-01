ffa76ff68952c031890a87ccf2b441064b2a91c6
Jeff <jeffreywjlim@gmail.com>
2025-02-01 15:26:29 -0500

    flexible boxes

--- src/pages/Debts.tsx
+++ src/pages/Debts.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import Banner from "../components/Banner";  
 import Sidebar from "../components/Sidebar"; 
 import Chatbot from "../components/Chatbot";
+import ExpandingDebtRectangleList from '../components/ExpandingDebtRectangleList';
+import "./Debts.css"
 
 const Debts: React.FC = () => {
+  const items: string[] = ["Rectangle 1", "Rectangle 2", "Rectangle 3"];
   return (
-    <>
+    <div className="debts-container">
       <Banner />
-      <Sidebar />
-      <Chatbot />
-    </>
+      <div className="content">
+        <Sidebar />
+        <ExpandingDebtRectangleList items={items} />
+        <Chatbot />
+      </div>
+    </div>
   );
 };
 
