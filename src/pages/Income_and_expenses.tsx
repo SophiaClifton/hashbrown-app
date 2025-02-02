93b762f7e244ebcbc298867d60e965b1f4744f0e
mpetruzziello <matthew.petruzziello@mail.mcgill.ca>
2025-02-02 01:14:43 -0500

    More audio

--- src/pages/Income_and_expenses.tsx
+++ src/pages/Income_and_expenses.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from "../components/Banner";
 import Chatbot from "../components/Chatbot";
 import Button from "../components/but";
@@ -6,6 +6,13 @@ import DoughnutCharts from "../components/DoughnutCharts";
 import "./Income_and_expenses.css"
 
 const Income_and_expenses: React.FC = () => {
+    useEffect(() => {
+        // Create and play the audio message when component mounts
+        const message = "Income and Expenses WebPage";
+        const utterance = new SpeechSynthesisUtterance(message);
+        window.speechSynthesis.speak(utterance);
+    }, []); // Empty dependency array means this runs once when component mounts
+
     return (
         <>
             <Banner />
